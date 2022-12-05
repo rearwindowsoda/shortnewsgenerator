b8f660b4b43ee1d28513fa9d7a21556f2c7d1880
Mateusz Owczarek <mateuszowczarek@onet.eu>
2022-12-05 17:06:51 +0100

    added express framework

--- app.ts
+++ app.ts
@@ -1,5 +1,10 @@
 import * as dotenv from 'dotenv';
+import express from 'express';
 
 if (process.env.NODE_ENV !== 'production') {
 	dotenv.config();
 }
+
+const app = express();
+
+app.listen(process.env.PORT || 3000);
